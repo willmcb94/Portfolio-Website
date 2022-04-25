edfe487d3ad84457a522e75de1d7e2bb14e56070
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-04-25 10:11:01 +0100

    update about

--- pages/components/About.js
+++ pages/components/About.js
@@ -16,23 +16,25 @@ export default function About() {
         <article className={styles.text_container}>
           <h2 className={styles.about_title}>About me</h2>
           <p className={styles.about_text}>
-            Hi I'm Will, I come from the rainy city (Manchester). I grew up as
-            the web became popular, seeing how it's grown and changed over the
-            past 20 years is nothing less than astonishing.<br></br>
+            Based in the vibrant (albeit rainy) city of Manchester, I matured
+            around the same time that the internet did - watching it evolve in
+            astonishing ways over the past 20 odd years.<br></br>
             <br></br>
-            This is what has inspired me to leave my stable career in sales to
-            become a developer! I’m extremely excited about what the future
-            holds for internet use and how I can be involved in this inovation
-            on my new career path.<br></br>
-            <br></br>I have graduated from the Northcoders fullstack developer
-            bootcamp and now I’m in search of my first developer role.<br></br>
+            Having always been struck by emerging web innovations, I became
+            motivated to work with the technologies that come next…<br></br>
             <br></br>
-            I’m keen to work for a company with a culture of learning where I
-            can really challenge myself to be better every day. I wish to build
-            my knowledge while creating code for the benefit of others.<br></br>
+            Driven by this passion to become a developer, I spent more evenings
+            than not buried in code, restlessly itching to leave my stable sales
+            career behind. And after graduating from the Northcoders Full Stack
+            Developer Bootcamp, I’m now ready for my first role.<br></br>
             <br></br>
-            If you have any questions or you are/know anybody in search of a
-            junior developer currently, please get in touch.
+            I’d love to land in a company with a culture of learning, where I
+            can showcase my acquired skill set while continuously bettering
+            myself. The challenge excites me - as does the ability to craft
+            great code that really makes a difference.<br></br>
+            <br></br>
+            Want to know more? Please don’t hesitate to get in touch about any
+            opportunities you may have for an eager junior developer.
           </p>
         </article>
         <div className={styles.img_container}>
